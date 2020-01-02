143a73aec716009ca4d3ca9418d8e6e78701a7d6
Jelena Peševski <jpesevski99@gmail.com>
2020-01-02 15:39:18 +0100

    Povezivanje sa bazom

---
+++ database.php
@@ -0,0 +1,54 @@
+<?php 
+include_once("config.php");
+$DB = mysqli_connect($DATABASE['host'], $DATABASE['username'], $DATABASE['password'], $DATABASE['database']);
+session_start();
+
+function check_login()
+{
+	if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
+
+		if ( (isset($_POST['username']) && isset($_POST['password'])) && $userid = admin_in_database($_POST['username'], $_POST['password'])) {
+		    $_SESSION['loggedin'] = true;
+		    $_SESSION['username'] = $_POST['username']; 
+		    $_SESSION['userid'] = $userid;
+		}else{
+			return false;
+		}
+	}
+	return true;
+}
+function login()
+{
+	if ( isset($_POST['username']) && isset($_POST['password'])){
+	 	if($userid = admin_in_database($_POST['username'], $_POST['password'])) {
+		    $_SESSION['loggedin'] = true;
+		    $_SESSION['username'] = $_POST['username']; 
+		    $_SESSION['userid'] = $userid;
+		    return 2;
+		}
+		return 1;
+	}
+	return 0;
+}
+
+
+function goto_page($page)
+{
+	header("Location: ".$page);
+	die();
+}
+
+
+function admin_in_database($username, $pass){
+	global $DB;
+	$username = mysqli_real_escape_string($DB, $username);
+	//$pass = md5($pass); ako budemo hashovali password-e
+	$pass = mysqli_real_escape_string($DB, $pass);
+	$result = $DB->query("SELECT 'korisnik_id' FROM `korisnici` WHERE `korisnik_ime`='$username' AND `korisnik_sifra`='$pass'");
+
+	if (mysqli_num_rows($result)!=1)return 0;
+	return $result->fetch_assoc()['korisnik_id'];
+}
+
+
+?>
