1e7c2f1ee06299283682ca0d73a42a23919c5eed
jelena-pesevski <jpesevski99@gmail.com>
2020-01-10 12:31:14 +0100

    dictionary

--- config.php
+++ config.php
@@ -3,6 +3,6 @@ $DATABASE = array(
 					'host' => "localhost",
 					'username' => "root",
 					'password' => "",
-					'database' => "dogadjaji", // cekamo dragana da napravi bazu ;) // dragan je napravio bazu - jej
+					'database' => "database_dogadjaji",
 					 );
  ?>
